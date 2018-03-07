ad3297ef53ee4785b340cd883bcbce03f0af0dee
Khouloud BELGUITH <khouloud.belguith@debian>
2018-03-07 16:51:04 +0100

    Delete products bulk action - HIGH TESTS

--- tests/E2E/test/selectors/BO/catalogpage/index.js
+++ tests/E2E/test/selectors/BO/catalogpage/index.js
@@ -8,6 +8,11 @@ module.exports = Object.assign(
       action_button: '(//*[@id="main-div"]//div[contains(@class, "bulk-catalog")]//a)[%ID]',
       green_validation: '//*[@id="main-div"]//div[contains(@class, "alert-success") and not(@style)]',
       product_status_icon: '(//*[@id="product_catalog_list"]//tbody/tr[%S]//i[contains(@class, "material-icons")])[1]',
+      product_name_input: '//*[@id="product_catalog_list"]/div[2]/div/table/thead/tr[2]/th[3]/input',
+      products_title_page: '//*[@id="main-div"]/div[1]/h2',
+      apply_button: '//*[@id="product_catalog_list"]/div[2]/div/table/thead/tr[2]/th[9]/button[1]',
+      delete_now_button: '//*[@id="catalog_deletion_modal"]/div/div/div[3]/button[2]',
+      reset_button: '//*[@id="product_catalog_list"]/div[2]/div/table/thead/tr[2]/th[9]/button[2]',
     }
   },
   require('./feature_submenu'),
